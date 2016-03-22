6d569abbc6877ce12f1ee0b8516789579d474fcd
Smruti Padhy <spadhy@illinois.edu>
2016-03-21 19:31:29 -0500

    examples updated and new example added for keys and tokens

--- examples_bdlib.r
+++ examples_bdlib.r
@@ -1,32 +1,36 @@
-source("path/to/bd.r")
-example.convert = function(){
-  dap            <- "dap-dev.ncsa.illinois.edu"
-  input_filename <- "path/to/filename"
-  output         <- "png" # the output format
-  output_path    <-"path/to/output"
-  wait           <- 80
-  output_file    <- browndog.convert(dap,input_filename,output,output_path,wait)
-  output_file
-}
+source("/path/to/bd.r")
 
-example.extractwithusernamepwd = function(){
-  file <- "path/to/file" #complete path to the file or file URL
-  wait <- 30
-  dts  <- "username:password@dts.ncsa.illinois.edu" # note: username of the format 'bd.user@gmail.com' should be converted to 'bd.user%40gmail.com'
-  key  <- ""
-  metadata   <- browndog.extract(dts,file,wait,key)
+#key <- browndog.getKey("https://bd-api.ncsa.illinois.edu/keys")
+#print(key)
+#browndog.saveKey(key)
+#token <- browndog.getToken("localhost:8080")
+#print(token)
+
+#outputs <- browndog.outputs("localhost:8080", "png")
+#print(outputs)
+
+example.convert = function(){
+  bds            <- "localhost:8080"
+  input_filename <- "path/to/inputfilename"
+  output         <- "png" # the output format
+  output_path    <-"/path/for/outputfile/"
+  wait           <- 80
+  output_file    <- browndog.convert(bds,input_filename,output,output_path,wait)
+  print(output_file)
+}
+#example.convert()
+
+example.bdextract = function(){
+  #file <- "path/to/file" #complete path to the file or file URL
+  wait <- 60
+  bds <- "localhost:8080" 
+  #bds <- "bd-api.ncsa.illinois.edu"
+  metadata   <- browndog.extract(bds,file,wait)
  }
-example.extractwithkey = function(){
-  file <- "path/to/file" #complete path to the file or file URL
-  wait <- 30
-  dts  <- "dts.ncsa.illinois.edu"
-  key  <- "DTS key"
-  metadata   <- browndog.extract(dts,file,wait,key)
- }
-metadata <- example.extractwithusernamepwd()
-metadata <- example.extractwithkey()
+
+metadata <- example.bdextract()
 #print(metadata$versusmetadata)
 print(metadata)
 
-#browndog.index("http://dts.ncsa.illinois.edu","/complete/path/directory/",120,key)
-#browndog.find("http://dts.ncsa.illinois.edu","complete/path/to/queryfile",60,key)
+#browndog.index("https://bd-api.ncsa.illinois.edu","/complete/path/directory/",120)
+#browndog.find("https://bd-api.ncsa.illinois.edu","complete/path/to/queryfile",60)
