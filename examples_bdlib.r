8f4e3124c8637cd929e96dcb11baebdcf1ecd41b
Smruti Padhy <spadhy@illinois.edu>
2016-07-27 14:48:04 -0500

    Updated the usage based on the new changes

--- examples_bdlib.r
+++ examples_bdlib.r
@@ -1,36 +1,52 @@
-source("/path/to/bd.r")
+#source("/path/to/bd.r")
 
-#key <- browndog.getKey("https://bd-api.ncsa.illinois.edu/keys")
-#print(key)
+#Obtain Brown Dog Access Key
+bds <- "https://bd-api.ncsa.illinois.edu"
+key <- browndog.getKey(bds)
+print(key)
+
+#Saving the key
 #browndog.saveKey(key)
-#token <- browndog.getToken("localhost:8080")
-#print(token)
 
-#outputs <- browndog.outputs("localhost:8080", "png")
+#Obtain and save an access token for the key
+token <- browndog.getToken("https://bd-api.ncsa.illinois.edu")
+#token <- browndog.getToken("localhost:8080")
+print(token)
+#browndog.saveToken(token)
+
+#Save both key and corresponsing token to a file for Brown Dog access
+browndog.saveKeyToken(key,token)
+
+#Obtain all possible output formats from Brown Dog
+#outputs <- browndog.outputs("bd-api.ncsa.illinois.edu", "png", token)
 #print(outputs)
 
-example.convert = function(){
-  bds            <- "localhost:8080"
+# Use Brown Dog conversion
+example.bdconvert = function(){
+  #bds            <- "localhost:8080"
+  bds            <- "bd-api.ncsa.illinois.edu"
   input_filename <- "path/to/inputfilename"
-  output         <- "png" # the output format
-  output_path    <-"/path/for/outputfile/"
+  output         <- "png" # an output format
+  output_path    <-"/path/for/outputfile/" # note the path ends with '/'
   wait           <- 80
-  output_file    <- browndog.convert(bds,input_filename,output,output_path,wait)
+  output_file    <- browndog.convert(bds,input_filename,output,output_path,token, wait)
   print(output_file)
 }
-#example.convert()
+#example.bdconvert()
 
+#Use Borwn Dog extraction 
 example.bdextract = function(){
-  #file <- "path/to/file" #complete path to the file or file URL
+  file <- "path/to/file" #complete path to the file or file URL
   wait <- 60
-  bds <- "localhost:8080" 
-  #bds <- "bd-api.ncsa.illinois.edu"
-  metadata   <- browndog.extract(bds,file,wait)
+  #bds <- "localhost:8080" 
+  bds <- "bd-api.ncsa.illinois.edu"
+  metadata   <- browndog.extract(bds,file,token,wait)
  }
-
-metadata <- example.bdextract()
+#metadata <- example.bdextract()
 #print(metadata$versusmetadata)
-print(metadata)
+#print(metadata)
 
-#browndog.index("https://bd-api.ncsa.illinois.edu","/complete/path/directory/",120)
-#browndog.find("https://bd-api.ncsa.illinois.edu","complete/path/to/queryfile",60)
+#Index and query a collection of files
+#browndog.index("bd-api.ncsa.illinois.edu","/complete/path/to/directory/",token,120)
+#browndog.find("bd-api.ncsa.illinois.edu","complete/path/to/queryfile",token,60)
+
